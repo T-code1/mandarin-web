20dfec150e40f4fca327f8925dabf7ade4f5e108
UmurerwaDivine <divinekagame@gmail.com>
2020-05-19 07:17:35 +0200

    contact all data needed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Explore from "./components/explore";
 import LandingPage from "./components/landingpage";
 import './App.css';
 import Services from './components/services';
+import Contact from './components/contact';
 const App = () => {
   return (
     <div>
       <LandingPage />
       <Explore />
+
     </div>
   )
 }
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import "./contact.css";
 const Contact = props => {
-  const {id} = props
+  const { id } = props
   return (
-    <div className="contact" id={id}>
-    <h1 h1 className="contact">This is contact </h1>;
+    <div className="container-fluid contact" id={id}>
+
+      <h2>Tell us what <br />
+       you are interested in
+       </h2>
+
+      <form action="/action_page.php">
+        <input type="text" id="name" name="Name" placeholder="Name" /><br />
+        <input type="email" id="email" name="Email" placeholder="Email" /><br />
+        <input type="text" id="message" name="Message" placeholder="message" /><br />
+        <button className="button">SEND</button>
+      </form>
+
     </div>
   )
- 
+
 }
- 
+
 export default Contact;

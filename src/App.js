52f2821fcc668442dcf9dba104b3cc490fb19060
UmurerwaDivine <divinekagame@gmail.com>
2020-05-23 01:12:17 +0200

    added all data in about(who we are)

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import LandingPage from "./components/landingpage";
 import './App.css';
 import Services from './components/services';
 import Contact from './components/contact';
+import About from './components/about';
 const App = () => {
   return (
     <div>
       <LandingPage />
-      <Explore />
 
     </div>
   )

779157e543525e3204cfe01ed2be1778063016bc
zachee <zacheeishimwe@gmail.com>
2020-06-22 08:40:36 +0200

    arrange our client images

--- src/components/client.js
+++ src/components/client.js
@@ -12,11 +12,11 @@ const Client =(props)=>{
             <img className="forex-img" src={require("../images/clients/forex.svg")} alt="clietn" />
             <img className="gemura-img" src={require("../images/clients/gemura.svg")} alt="clietn" />
             <img className="huzamart-img" src={require("../images/clients/HuzaMart.svg")} alt="clietn" />
-            <img className="Isange-img" src={require("../images/clients/Isange_group.svg")} alt="clietn" />
             <img className="Kwetu-img" src={require("../images/clients/Kwetu-pazuri.svg")} alt="clietn" />
             <img className="osisolution-img" src={require("../images/clients/osiSolution.svg")} alt="clietn" />
             <img className="solid-img" src={require("../images/clients/solid.svg")} alt="clietn" />;
             <img className="wivunika-img" src={require("../images/clients/wivunika.svg")} alt="clietn" />
+            <img className="Isange-img" src={require("../images/clients/Isange_group.svg")} alt="clietn" />
             </div>
         </div>
     )

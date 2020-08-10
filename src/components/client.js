4b8007526d738ccc098b9e848e1fd29008063266
UmurerwaDivine <divinekagame@gmail.com>
2020-08-10 03:11:08 +0200

    edit everypage

--- src/components/client.js
+++ src/components/client.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./client.css";
 
-const Client =(props)=>{
+const Client = (props) => {
     const { id } = props
-    return(
-        <div ClassName="client" id={id}>
- 
-            
+    return (
+        <div ClassName=" container-fluid client" id={id}>
+
+
             <div className="client_link">
-            <img className="chickentStore-img" src={require("../images/clients/Chickenstore.svg")} alt="clietn" />
-            <img className="forex-img" src={require("../images/clients/forex.svg")} alt="clietn" />
-            <img className="huzamart-img" src={require("../images/clients/HuzaMart.svg")} alt="clietn" />
-            <img className="Kwetu-img" src={require("../images/clients/Kwetu-pazuri.svg")} alt="clietn" />
-            <img className="osisolution-img" src={require("../images/clients/osiSolution.svg")} alt="clietn" />
-            <img className="solid-img" src={require("../images/clients/solid.svg")} alt="clietn" />;
+                <h2>OUR CLIENTS</h2>
+                <div className="images">
+                    <img className="chickentStore-img" src={require("../images/clients/Chickenstore.svg")} alt="clietn" />
+                    <img className="forex-img" src={require("../images/clients/forex.svg")} alt="clietn" />
+                    <img className="huzamart-img" src={require("../images/clients/HuzaMart.svg")} alt="clietn" />
+                    <img className="Kwetu-img" src={require("../images/clients/Kwetu-pazuri.svg")} alt="clietn" />
+                    <img className="osisolution-img" src={require("../images/clients/osiSolution.svg")} alt="clietn" />
+                    <img className="solid-img" src={require("../images/clients/solid.svg")} alt="clietn" />;
             <img className="wivunika-img" src={require("../images/clients/wivunika.svg")} alt="clietn" />
-            <img className="Isange-img" src={require("../images/clients/Isange_group.svg")} alt="clietn" />
-            {/* <img className="gemura-img" src={require("../images/clients/gemura.svg")} alt="clietn" /> */}
+                    <img className="Isange-img" src={require("../images/clients/Isange_group.svg")} alt="clietn" />
+                    {/* <img className="gemura-img" src={require("../images/clients/gemura.svg")} alt="clietn" /> */}
+                </div>
             </div>
         </div>
     )
--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import "./home.css";
 import Aos from 'aos';
 import 'aos/dist/aos.css';
@@ -7,18 +7,18 @@ import 'aos/dist/aos.css';
 const Home = props => {
   const { id } = props
 
-  useEffect(()=>{
-    Aos.init({duration:2000})
-  },[])
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
 
 
-  return(
+  return (
     <div className="home" id={id}>
-    <div className="home_text" data-aos="fade-right" >
-    <p><span>We</span> are here</p>
-    <p>to <span>Serve</span> you and </p>
-     <p> bring <span>you</span>r ideas to life .</p>
-    </div>
+      <div className="home_text" data-aos="fade-right" >
+        <p><span>We</span> are here</p>
+        <p>to <span>Serve</span> you and </p>
+        <p> bring <span>you</span>r ideas to life<span> .</span></p>
+      </div>
     </div>
   )
 }

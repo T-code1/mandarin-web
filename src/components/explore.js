45f32ec6d6bd58dbd479601fa6fddd4057c959f6
zachee <zacheeishimwe@gmail.com>
2020-08-13 21:41:24 +0200

    fix the navigations explore

--- src/components/explore.js
+++ src/components/explore.js
@@ -1,267 +1,201 @@
 import React, { useState } from 'react';
 import Footer from './footer';
 import './explore.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Row,
-  Col
-} from 'reactstrap';
-import { Link } from "react-scroll";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+// import { Link } from "react-scroll";
 
 const logStyles = {
-  height: 40,
-  width: 140,
-  marginTop: 40,
-  //  position: 'fixed'
-}
+	height: 40,
+	width: 140,
+	marginTop: 40
+	//  position: 'fixed'
+};
 
 const navStyles = {
-  cursor: "pointer",
-  fontSize: 14,
-  fontWeight: 8,
-  padding: 2,
-  marginTop: "3%",
-  marginRight: "10%",
-  // position:'fixed'
-}
+	cursor: 'pointer',
+	fontSize: 14,
+	fontWeight: 8,
+	padding: 2,
+	marginTop: '3%',
+	marginRight: '10%'
+	// position:'fixed'
+};
 
 const Explore = (props) => {
-  const { id } = props
-  const [isOpen, setIsOpen] = useState(false);
+	const { id } = props;
+	const [ isOpen, setIsOpen ] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
-  return (
-    <div>
-      <div id={id}>
-        <Row>
-          <Col xs="8">
-            <NavbarBrand href="/"><img style={logStyles} className="logo" src={require("../images/Official_Logo.png")} alt="pixels" /></NavbarBrand>
-          </Col>
-          <Col >
-            <Navbar light expand="md" >
-              <NavbarToggler onClick={toggle} />
-              <Collapse isOpen={isOpen} navbar>
-                <Nav className="mr-auto" navbar style={navStyles}>
+	const toggle = () => setIsOpen(!isOpen);
+	return (
+		<div>
+			<div id={id}>
+				<Row>
+					<Col xs="8">
+						<NavbarBrand href="/">
+							<img
+								style={logStyles}
+								className="logo"
+								src={require('../images/Official_Logo.png')}
+								alt="pixels"
+							/>
+						</NavbarBrand>
+					</Col>
+					<Col>
+						<Navbar light expand="md">
+							<NavbarToggler onClick={toggle} />
+							<Collapse isOpen={isOpen} navbar>
+								<Nav className="mr-auto" navbar style={navStyles}>
+									<NavItem>
+										<NavLink>
+											<Link to="/home">Home</Link>
+										</NavLink>
+									</NavItem>
 
-                  <NavItem>
-                    <NavLink>
-                      <Link
-                        activeClass="active"
-                        to="section1"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                      >
-                        Home
-              </Link>
-                    </NavLink>
-                  </NavItem>
+									<NavItem>
+										<NavLink>
+											<Link to="/services">Services</Link>
+										</NavLink>
+									</NavItem>
 
-                  {/* This is the first navigation */}
+									<NavItem>
+										<NavLink>
+											<Link to="/client">Client</Link>
+										</NavLink>
+									</NavItem>
 
-                  <NavItem>
-                    <NavLink>
-                      <Link
-                        activeClass="active"
-                        to="section2"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
+									<NavItem>
+										<NavLink>
+											<Link to="/about">About</Link>
+										</NavLink>
+									</NavItem>
 
-                      >
-                        Services
-              </Link>
-                    </NavLink>
-                  </NavItem>
+									<NavItem>
+										<NavLink>
+											<Link to="/contact">Contact</Link>
+										</NavLink>
+									</NavItem>
+								</Nav>
+							</Collapse>
+						</Navbar>
+					</Col>
+				</Row>
+			</div>
 
+			<div />
 
-                  {/* This is the first navigation */}
+			<div className="explore-container">
+				<div className="texts">
+					<p className="toServe">HERE TO SERVE</p>
+					<p className="text">Our native eye to detail continously pushes</p>
+					<p className="text">boundary to help you reach your reach your audience effortly</p>
+				</div>
+				<div className="col explores">
+					<div className="row firstRow">
+						<div className="rowOne">
+							<div className="startegyImage">
+								<img src={require('../images/explore/strategy.svg')} alt="img" id="stategy" />
+							</div>
+							<div className="strategyText">
+								<div className="strategyTitle">
+									<h4>Digital Strategy</h4>
+								</div>
+								<div className="strategyPragraph">
+									<p>Through well-planned campains with anlytics</p>
+									<p>we open up the digital world to your outstanding business</p>
+									<p>we believe every business is outstanding bu some lack</p>
+									<p>a custimezed digtal campaign to prove.</p>
+								</div>
+							</div>
+						</div>
 
-                  <NavItem>
-                    <NavLink>
-                      <Link
-                        activeClass="active"
-                        to="section3"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                      >
-                        About
-              </Link>
-                    </NavLink>
-                  </NavItem>
+						<div className="rowOne">
+							<div className="startegyImage">
+								<img src={require('../images/explore/computeradvetise.svg')} alt="img" id="stategy" />
+							</div>
+							<div className="strategyText">
+								<div className="strategyTitle">
+									<h4>Graphic Design and Content Creation</h4>
+								</div>
+								<div className="strategyPragraph">
+									<p>The content we create embodies your brand,resonates with your</p>
+									<p>audience which is then complemented withtasteful graphics to</p>
+									<p>consistely make you stand oout in a saturated platform.</p>
+								</div>
+							</div>
+						</div>
 
+						<div className="rowOne">
+							<div className="startegyImage">
+								<img src={require('../images/explore/qr-code.svg')} alt="img" id="stategy" />
+							</div>
+							<div className="strategyText">
+								<div className="strategyTitle">
+									<h4>App and Software Technology</h4>
+								</div>
+								<div className="strategyPragraph">
+									<p>Carefully designed and specifically curated for your product</p>
+									<p>or services,our compeetitive apps will ultimately</p>
+									<p>help you reach your marketing goals</p>
+								</div>
+							</div>
+						</div>
+					</div>
 
-                  <NavItem>
-                    <NavLink>
-                      <Link
-                        activeClass="active"
-                        to="section4"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                      >
-                        Client
-              </Link>
-                    </NavLink>
-                  </NavItem>
+					<div className="row firstRow">
+						<div className="rowOne">
+							<div className="startegyImage">
+								<img src={require('../images/explore/world-wide-web.svg')} alt="img" id="stategy" />
+							</div>
+							<div className="strategyText">
+								<div className="strategyTitle">
+									<h4>Website and Micro-Sites</h4>
+								</div>
+								<div className="strategyPragraph">
+									<p>Custom designed websites with a range of features is what your</p>
+									<p>business is missing in order to stand out ,We design a wide range</p>
+									<p>of professional websites that will bring your project to completion</p>
+								</div>
+							</div>
+						</div>
 
+						<div className="rowOne">
+							<div className="startegyImage">
+								<img src={require('../images/explore/seo.svg')} alt="img" id="stategy" />
+							</div>
+							<div className="strategyText">
+								<div className="strategyTitle">
+									<h4>SEM and Digital Advertising</h4>
+								</div>
+								<div className="strategyPragraph">
+									<p>Our extensive knowledge of key-word strategy and latest digital</p>
+									<p>advertisement tools simply yet effectively dilivers high quality</p>
+									<p>advertisements.</p>
+								</div>
+							</div>
+						</div>
 
-                  <NavItem>
-                    <NavLink >
-                      <Link
-                        activeClass="active"
-                        to="section5"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                      >
-                        Contact
-              </Link>
-                    </NavLink>
-                  </NavItem>
-
-                </Nav>
-              </Collapse>
-            </Navbar>
-          </Col>
-        </Row>
-      </div>
-
-
-
-      <div>
-
-      </div>
-
-      <div className="explore-container">
-
-        <div className="texts">
-          <p className="toServe">HERE TO SERVE</p>
-          <p className="text">Our native eye to detail continously pushes</p>
-          <p className="text">boundary to help you reach your reach your audience effortly</p>
-        </div>
-        <div className="col explores">
-          <div className="row firstRow">
-            <div className="rowOne">
-              <div className="startegyImage">
-                <img src={require('../images/explore/strategy.svg')} alt="img" id="stategy" />
-              </div>
-              <div className="strategyText">
-                <div className="strategyTitle">
-                  <h4>Digital Strategy</h4>
-                </div>
-                <div className="strategyPragraph">
-                  <p>Through well-planned campains with anlytics</p>
-                  <p>we open up the digital world to your outstanding business</p>
-                  <p>we believe every business is outstanding bu some lack</p>
-                  <p>a custimezed digtal campaign to prove.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="rowOne">
-              <div className="startegyImage">
-                <img src={require('../images/explore/computeradvetise.svg')} alt="img" id="stategy" />
-              </div>
-              <div className="strategyText">
-                <div className="strategyTitle">
-                  <h4>Graphic Design and Content Creation</h4>
-                </div>
-                <div className="strategyPragraph">
-                  <p>The content we create embodies your brand,resonates with your</p>
-                  <p>audience which is then complemented withtasteful graphics to</p>
-                  <p>consistely make you stand oout in a saturated platform.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="rowOne">
-              <div className="startegyImage">
-                <img src={require('../images/explore/qr-code.svg')} alt="img" id="stategy" />
-              </div>
-              <div className="strategyText">
-                <div className="strategyTitle">
-                  <h4>App and Software Technology</h4>
-                </div>
-                <div className="strategyPragraph">
-                  <p>Carefully designed and specifically curated for your product</p>
-                  <p>or services,our compeetitive apps will ultimately</p>
-                  <p>help you reach your marketing goals</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-
-          <div className="row firstRow">
-            <div className="rowOne">
-              <div className="startegyImage">
-                <img src={require('../images/explore/world-wide-web.svg')} alt="img" id="stategy" />
-              </div>
-              <div className="strategyText">
-                <div className="strategyTitle">
-                  <h4>Website and Micro-Sites</h4>
-                </div>
-                <div className="strategyPragraph">
-                  <p>Custom designed websites with a range of features is what your</p>
-                  <p>business is missing in order to stand out ,We design a wide range</p>
-                  <p>of professional websites that will bring your project to completion</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="rowOne">
-              <div className="startegyImage">
-                <img src={require('../images/explore/seo.svg')} alt="img" id="stategy" />
-              </div>
-              <div className="strategyText">
-                <div className="strategyTitle">
-                  <h4>SEM and Digital Advertising</h4>
-                </div>
-                <div className="strategyPragraph">
-                  <p>Our extensive knowledge of key-word strategy and latest digital</p>
-                  <p>advertisement tools simply yet effectively dilivers high quality</p>
-                  <p>advertisements.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="rowOne">
-              <div className="startegyImage">
-                <img src={require('../images/explore/seo.svg')} alt="img" id="stategy" />
-              </div>
-              <div className="strategyText">
-                <div className="strategyTitle">
-                  <h4>SEO</h4>
-                </div>
-                <div className="strategyPragraph">
-                  <p>We offer SEO (Search Engine Optimization) services that are</p>
-                  <p>result-driven and meticulously nurture you brand visiblity</p>
-                  <p>and audience reach.Isange-img</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+						<div className="rowOne">
+							<div className="startegyImage">
+								<img src={require('../images/explore/seo.svg')} alt="img" id="stategy" />
+							</div>
+							<div className="strategyText">
+								<div className="strategyTitle">
+									<h4>SEO</h4>
+								</div>
+								<div className="strategyPragraph">
+									<p>We offer SEO (Search Engine Optimization) services that are</p>
+									<p>result-driven and meticulously nurture you brand visiblity</p>
+									<p>and audience reach.Isange-img</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Explore;
--- src/components/services.js
+++ src/components/services.js
@@ -1,35 +1,35 @@
 import React, { useEffect } from 'react';
-import "./services.css";
+import './services.css';
 import { Link } from 'react-router-dom';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-const Services = props => {
-  const { id } = props
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, [])
+const Services = (props) => {
+	const { id } = props;
+	useEffect(() => {
+		Aos.init({ duration: 2000 });
+	}, []);
 
-  return (
-    <div className="container-fluid services" id={id}>
+	return (
+		<div className="container-fluid services" id={id}>
+			<div className="row content">
+				<div data-aos="fade-right" className="col-sm-6 image">
+					<img className="image" src={require('../images/MacBook Pro 16.png')} alt="MacBook Pro 16" />
+				</div>
 
-      <div className="row content">
-        <div data-aos="fade-right" className="col-sm-6 image">
-          <img className="image" src={require("../images/MacBook Pro 16.png")} alt="MacBook Pro 16" />
-        </div>
-
-        <div data-aos="fade-left" className=" title">
-          <p id="head">SERVICES</p>
-          <p>" Simplicity is not the goal, it is by-product<br />
-            of a good idea and modest expectations."<br />
-            -Paul Rand
-            </p>
-          <Link to="/explore" id="pexplore">Explore > </Link>
-        </div>
-
-      </div>
-
-    </div>
-  )
-}
+				<div data-aos="fade-left" className=" title">
+					<p id="head">SERVICES</p>
+					<p>
+						" Simplicity is not the goal, it is by-product<br />
+						of a good idea and modest expectations."<br />
+						-Paul Rand
+					</p>
+					<Link to="/explore" id="pexplore">
+						Explore>{' '}
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Services;

4108707d00a6d474e3d35509a65524b02bdc784b
zachee <zacheeishimwe@gmail.com>
2020-08-25 18:48:07 +0200

    make navigation

--- src/components/exploreNav/navbar.js
+++ src/components/exploreNav/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Burger from './Burger'
+import Burger from './Burger';
 
 const Nav = styled.nav`
 	width: 100%;
@@ -8,23 +8,27 @@ const Nav = styled.nav`
 	/* border-bottom: 2px solid #f1f1f1; */
 	padding: 2px 20px;
 	display: flex;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
 	justify-content: space-between;
-	.logo img{
+	z-index: 200;
+	background-color: white;
+	.logo img {
 		padding: 15px 0;
-		width:120px;
+		width: 120px;
 		height: 65px;
-
 	}
-
-	
 `;
 
 const Navbar = () => {
 	return (
 		<Nav>
-			<div className="logo"><img src={require('../../images/Official_Logo.png')} alt="Mandarin" id="mandarin" /></div>
+			<div className="logo">
+				<img src={require('../../images/Official_Logo.png')} alt="Mandarin" id="mandarin" />
+			</div>
 			<Burger />
-
 		</Nav>
 	);
 };
--- src/components/homeNav/navbar.js
+++ src/components/homeNav/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Burger from './Burger'
+import Burger from './Burger';
 import { Link } from 'react-scroll';
 
 const Nav = styled.nav`
@@ -9,25 +9,30 @@ const Nav = styled.nav`
 	/* border-bottom: 2px solid #f1f1f1; */
 	padding: 2px 20px;
 	display: flex;
-	/* position:fixed; */
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
 	justify-content: space-between;
-	.logo img{
+	z-index: 200;
+	background-color: white;
+	.logo img {
 		padding: 15px 0;
-		width:120px;
+		width: 120px;
 		height: 65px;
-		cursor:pointer;
-
+		cursor: pointer;
 	}
-
-	
 `;
 
 const Navbar = () => {
 	return (
 		<Nav>
-			<div className="logo"><Link to="/"><img src={require('../../images/Official_Logo.png')} alt="Mandarin" id="mandarin" /></Link></div>
+			<div className="logo">
+				<Link to="/">
+					<img src={require('../../images/Official_Logo.png')} alt="Mandarin" id="mandarin" />
+				</Link>
+			</div>
 			<Burger />
-
 		</Nav>
 	);
 };

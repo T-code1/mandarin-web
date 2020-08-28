eadf412102dedb4238c6ee3c106dfa96d8cb7bde
zachee <zacheeishimwe@gmail.com>
2020-08-28 08:06:52 +0200

    Updates

---
+++ precache-manifest.3500c5549fde701c56b82b6f762c909c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1a8a54d667915642078b7cabd91a05",
+    "url": "/mandarin-web/index.html"
+  },
+  {
+    "revision": "201b8e141babfc38ff4c",
+    "url": "/mandarin-web/static/css/2.98432953.chunk.css"
+  },
+  {
+    "revision": "f6df3748dd4dcfceed6b",
+    "url": "/mandarin-web/static/css/main.b305ade1.chunk.css"
+  },
+  {
+    "revision": "201b8e141babfc38ff4c",
+    "url": "/mandarin-web/static/js/2.2de415b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mandarin-web/static/js/2.2de415b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6df3748dd4dcfceed6b",
+    "url": "/mandarin-web/static/js/main.8d87a90b.chunk.js"
+  },
+  {
+    "revision": "0ab3d864926cf60f99b5",
+    "url": "/mandarin-web/static/js/runtime-main.885684f1.js"
+  },
+  {
+    "revision": "6354fc2af87cb1302eb469deaa24f9ee",
+    "url": "/mandarin-web/static/media/02.6354fc2a.png"
+  },
+  {
+    "revision": "3ed3346f9c96f53dc0fac5fe1bf9bb73",
+    "url": "/mandarin-web/static/media/03.3ed3346f.png"
+  },
+  {
+    "revision": "0126a070eeca0a7786bf5a06755db11d",
+    "url": "/mandarin-web/static/media/04.0126a070.png"
+  },
+  {
+    "revision": "1cfe0390b726b15359ddd4f3f724c75c",
+    "url": "/mandarin-web/static/media/ChaletBook.1cfe0390.ttf"
+  },
+  {
+    "revision": "92b236fe0c29d59d14fe5d947e9cf3da",
+    "url": "/mandarin-web/static/media/ChaletBookBold.92b236fe.ttf"
+  },
+  {
+    "revision": "cfae073ddc50d9e2601ed7e929d8700e",
+    "url": "/mandarin-web/static/media/Chickenstore.cfae073d.svg"
+  },
+  {
+    "revision": "5c9435d632be6306fcb093baa8a36ff8",
+    "url": "/mandarin-web/static/media/HuzaMart.5c9435d6.svg"
+  },
+  {
+    "revision": "ff4d34f668bab5b2028b4c30c9c20851",
+    "url": "/mandarin-web/static/media/Intro Bold Regular.ff4d34f6.otf"
+  },
+  {
+    "revision": "295053dd4607256c69928422da4107cf",
+    "url": "/mandarin-web/static/media/Isange_group.295053dd.svg"
+  },
+  {
+    "revision": "d959d979c0472bd8c64a49841f873679",
+    "url": "/mandarin-web/static/media/Kwetu-pazuri.d959d979.svg"
+  },
+  {
+    "revision": "a897befae322745bd34af92ef4482f8a",
+    "url": "/mandarin-web/static/media/MacBook Pro 16.a897befa.png"
+  },
+  {
+    "revision": "b33c2541ad3be73a3f457a2d84df91c8",
+    "url": "/mandarin-web/static/media/Official_Logo.b33c2541.png"
+  },
+  {
+    "revision": "58da367097cb6fdf2a259e130bad2606",
+    "url": "/mandarin-web/static/media/computeradvetise.58da3670.svg"
+  },
+  {
+    "revision": "387bca24ed8401316ed9a13b592e4cd7",
+    "url": "/mandarin-web/static/media/forex.387bca24.svg"
+  },
+  {
+    "revision": "7a312f8e8688cdd40869422f4556fb59",
+    "url": "/mandarin-web/static/media/osiSolution.7a312f8e.svg"
+  },
+  {
+    "revision": "72c1686259dbd8a5e3a93b7fe5507da0",
+    "url": "/mandarin-web/static/media/qr-code.72c16862.svg"
+  },
+  {
+    "revision": "795fc611afa4493cf1f8780a48d92f73",
+    "url": "/mandarin-web/static/media/seo.795fc611.svg"
+  },
+  {
+    "revision": "c8ad212ed649b4691ad3e01987a832c1",
+    "url": "/mandarin-web/static/media/solid.c8ad212e.svg"
+  },
+  {
+    "revision": "f900554e4ecca6884b49cd6801ca9cb4",
+    "url": "/mandarin-web/static/media/strategy.f900554e.svg"
+  },
+  {
+    "revision": "7e31feb07eb5c5acc50201813a02bd37",
+    "url": "/mandarin-web/static/media/wivunika.7e31feb0.svg"
+  },
+  {
+    "revision": "f3e3be1e99e296cf7f323ce36a36e9ac",
+    "url": "/mandarin-web/static/media/world-wide-web.f3e3be1e.svg"
+  }
+]);

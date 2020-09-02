7b49d1b510077c819202dd4b4892bf8ae30ef44e
zachee <zacheeishimwe@gmail.com>
2020-09-02 21:28:17 +0200

    add email notification

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,14 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './contact.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import Footer from './footer';
 const Contact = (props) => {
-	console.log('hello world ');
+	const [ form, setForm ] = useState({ name: '', email: '', message: '' });
+
 	const { id } = props;
 	useEffect(() => {
 		Aos.init({ duration: 2000 });
 	}, []);
+
+	const handleChange = (event) => {
+		setForm({ ...form, [event.target.name]: event.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.post('https://formcarry.com/s/X2paGXRQM90k', form, { headers: { Accept: 'application/json' } })
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+
+		setForm({ name: '', email: '', message: '' });
+	};
 	return (
 		<React.Fragment>
 			<div className="container-fluid contact" id={id}>
@@ -17,14 +38,41 @@ const Contact = (props) => {
 					you are interested in.
 				</p>
 
-				<form data-aos="fade-up" action="/email">
-					<input type="text" id="name" name="Name" placeholder="Name" />
+				<form data-aos="fade-up" onSubmit={handleSubmit}>
+					<input
+						type="text"
+						name="name"
+						id="name"
+						value={form.name}
+						placeholder="name"
+						onChange={handleChange}
+					/>
 					<br />
-					<input type="email" id="email" name="Email" placeholder="Email" />
+
+					<input
+						type="email"
+						id="email"
+						name="email"
+						value={form.email}
+						placeholder="Email"
+						onChange={handleChange}
+					/>
 					<br />
-					<input type="text" id="message" name="Message" placeholder="message" />
-					<br />
-					<button className="button">SEND</button>
+
+					<input
+						type="text"
+						name="message"
+						id="message"
+						value={form.message}
+						placeholder="message"
+						onChange={handleChange}
+					/>
+
+					<div>
+						<button type="submit" className="button">
+							SEND
+						</button>
+					</div>
 				</form>
 			</div>
 			<Footer />
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,58 +1,90 @@
-import React from 'react';
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
-import "./footer.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import './footer.css';
 import { Link } from 'react-router-dom';
 const Footer = () => {
-   return (
-      <React.Fragment>
-         <div className="footer">
-            <div className="column1">
-               <h6>About Us</h6>
-               <p id="text1">A multi-faced company that offers an array</p>
-               <p id="text1">of digital, advertising and web developments services</p>
-            </div>
+	const [ form, setForm ] = useState({ email: '' });
 
-            <div className="column2">
-               <h6> More About Us</h6>
-               <p id="text1">Team</p>
-               <p id="text1">Our Works</p>
-               <p id="text1">Services</p>
-               <p id="text1">Jobs</p>
-            </div>
+	const handleChange = (event) => {
+		setForm({ ...form, [event.target.name]: event.target.value });
+	};
 
-            <div className="column3">
-               <h6>Contact</h6>
-               <p id="text1">+(250)784658958</p>
-               <p id="text1">info@madarinagency digital</p>
-               <p id="text1">Kigali Rwanda</p>
-            </div>
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.post('https://formcarry.com/s/X2paGXRQM90k', form, { headers: { Accept: 'application/json' } })
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
 
-            <div className="column4 ">
-               <h6>Stay Connected</h6>
-               <p id="text4">Subscribe to our Newsletter</p>
-               <div className="email_input">
-                  <input type="email" id="emailInput" name="Email" placeholder="Email address" />
-                  <p id="button">SUBSCRIBE</p>
+		setForm({ email: '' });
+	};
 
-               </div>
-               <div className='socialMedia'>
-                  <Link to=""><FaFacebookF id="fb" /></Link>
-                  <Link to=""><FaTwitter id="fb" /></Link>
-                  <Link to=""><FaInstagram id="fb" /></Link>
-               </div>
-            </div>
-         </div>
-         <div className="copy">
-            <div className="line">
+	return (
+		<React.Fragment>
+			<div className="footer">
+				<div className="column1">
+					<h6>About Us</h6>
+					<p id="text1">A multi-faced company that offers an array</p>
+					<p id="text1">of digital, advertising and web developments services</p>
+				</div>
 
-            </div>
+				<div className="column2">
+					<h6> More About Us</h6>
+					<p id="text1">Team</p>
+					<p id="text1">Our Works</p>
+					<p id="text1">Services</p>
+					<p id="text1">Jobs</p>
+				</div>
 
-            <p> &copy; 2020 Copyright , All rights reserved.</p>
+				<div className="column3">
+					<h6>Contact</h6>
+					<p id="text1">+(250)784658958</p>
+					<p id="text1">info@madarinagency digital</p>
+					<p id="text1">Kigali Rwanda</p>
+				</div>
 
-         </div>
+				<div className="column4 ">
+					<h6>Stay Connected</h6>
+					<p id="text4">Subscribe to our Newsletter</p>
+					<form onSubmit={handleSubmit}>
+						<div className="email_input">
+							<input
+								type="email"
+								id="emailInput"
+								name="email"
+								value={form.email}
+								placeholder="Email"
+								onChange={handleChange}
+							/>
+							<button id="button">SUBSCRIBE</button>
+						</div>
+					</form>
+					<div className="socialMedia">
+						<Link to="">
+							<FaFacebookF id="fb" />
+						</Link>
+						<Link to="">
+							<FaTwitter id="fb" />
+						</Link>
+						<Link to="">
+							<FaInstagram id="fb" />
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className="copy">
+				<div className="line" />
 
-      </React.Fragment>
-   )
-}
+				<p> &copy; 2020 Copyright , All rights reserved.</p>
+			</div>
+		</React.Fragment>
+	);
+};
 
 export default Footer;
